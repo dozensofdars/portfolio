39a414dac0175fcfd720592c5a3027eb552370f8
dozensofdars <yuuya4000s@gmail.com>
2020-01-30 13:52:38 +0000

    Updates

---
+++ precache-manifest.b16607b4ec20840a7291099f4b9b3206.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d623b480fbffe6e77a991beeeb994c2e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6b1795641904815f200f",
+    "url": "/portfolio/static/js/2.46a35fb5.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/portfolio/static/js/2.46a35fb5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09242c05086f4c679e68",
+    "url": "/portfolio/static/js/main.6c9fd0cf.chunk.js"
+  },
+  {
+    "revision": "6fef3673099b4dcdb973",
+    "url": "/portfolio/static/js/runtime-main.fed7eca6.js"
+  },
+  {
+    "revision": "f1367396b2a6a95268fa9a8f6ea60a5d",
+    "url": "/portfolio/static/media/danshikoh.f1367396.png"
+  },
+  {
+    "revision": "cefc20232703e5e3c24efd5f50d75e26",
+    "url": "/portfolio/static/media/github.cefc2023.png"
+  },
+  {
+    "revision": "5305776f86d37a15bf7f45c4dae4afdc",
+    "url": "/portfolio/static/media/header.5305776f.jpg"
+  },
+  {
+    "revision": "ef34f20849e00b3d0bc4bd1bf7afcc6f",
+    "url": "/portfolio/static/media/nodered.ef34f208.png"
+  },
+  {
+    "revision": "2825e10a7fa992082ffd3c289bf9c14d",
+    "url": "/portfolio/static/media/portfolio.2825e10a.png"
+  },
+  {
+    "revision": "1f845b47aee3fc2b662c4e6df1e88f08",
+    "url": "/portfolio/static/media/vocaminer.1f845b47.png"
+  }
+]);
